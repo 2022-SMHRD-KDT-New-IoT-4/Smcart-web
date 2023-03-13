8f6f0bd4ff087a55ce0c9cc736d64a1e460a5fef
poppedpigeon <ripcurr3nt@gmail.com>
2023-03-13 21:49:50 +0900

    qr이안됨

--- cart/src/main/java/com/cart/controller/Main.java
+++ cart/src/main/java/com/cart/controller/Main.java
@@ -1,14 +1,17 @@
 package com.cart.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.cart.model.QrDAO;
 
 public class Main implements Command {
 	private static final long serialVersionUID = 1L;
@@ -16,17 +19,43 @@ public class Main implements Command {
 
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+//		  StringBuffer jb = new StringBuffer();
+//		  String line = null;
+//		  try {
+//		    BufferedReader reader = request.getReader();
+//		    while ((line = reader.readLine()) != null)
+//		      jb.append(line);
+//		  } catch (Exception e) { /*report an error*/ }
+//
+//		  try {
+//			  JSONParser parser = new JSONParser();
+//			  JSONObject jsonObject = (JSONObject) parser.parse(jb.toString());			 
+//			  System.out.println("값만 >> "+jsonObject.get("path"));
+//		  } catch (Exception e) {
+//		    // crash and burn
+//		    throw new IOException("Error parsing JSON request string");
+//		  }
+//		
+		
 		String qr_code = request.getParameter("qr_code");
-	
-		if(qr_code != null) {
-			System.out.println("모듈" + qr_code);
-			
-		}else {
-			System.out.println("값이 없습니다.");
-		}
+//		String data = request.getParameter("path");
+		QrDAO dao = new QrDAO();
+		int row = dao.SelectQr(qr_code);
+//		System.out.println(row);
+		
+		PrintWriter out = response.getWriter();
+	    out.print(row);
+	    System.out.println(row);
+//		if(qr_code != null) {
+//			System.out.println(qr_code);
+//			
+//		}else {
+//			System.out.println("값이 없습니다.");
+//		}
+//		
 		
 		
-		return "main.jsp";
+		return "Main.jsp";
 	}
 
 }
